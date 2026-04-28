ea4359289bedfc7ef23a1fc060c05b5ce1c4ea2b
jupiteresia <teresia.constantine@gmail.com>
2026-04-28 23:44:43 +0300

    level design pt2

--- Assets/Assets/Scripts/collectible.cs
+++ Assets/Assets/Scripts/collectible.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class collectible : MonoBehaviour
 {
-    public int scoreValue = 10;   // change this per coin if you want some worth more
+    public int scoreValue = 10;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            ScoreManager.instance.AddScore(scoreValue);
-            SFXManager.Instance.Playblinklesound();
+            if (ScoreManager.instance != null)
+                ScoreManager.instance.AddScore(scoreValue);
+            else
+                Debug.LogWarning("ScoreManager instance is missing from the scene!");
+
+            if (SFXManager.Instance != null)
+                SFXManager.Instance.Playblinklesound();
+            else
+                Debug.LogWarning("SFXManager instance is missing from the scene!");
+
             Destroy(gameObject);
         }
     }
