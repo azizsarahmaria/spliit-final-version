4da43e6a1ab2735f1f814c4d4c86b5ce57d2420e
jupiteresia <teresia.constantine@gmail.com>
2026-04-28 12:21:03 +0300

    fixed mushroom script AGAIN

--- Assets/Assets/Scripts/BouncyMushroom.cs
+++ Assets/Assets/Scripts/BouncyMushroom.cs
@@ -21,26 +21,22 @@ public class BouncyMushroom : MonoBehaviour
         {
             Vector2 contactNormal = collision.contacts[0].normal;
 
-            if (contactNormal.y < -0.5f)
+            if (contactNormal.y < -0.5f) // ✅ negative for this mushroom
             {
                 Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
                 player playerScript = collision.gameObject.GetComponent<player>();
 
                 if (rb != null && playerScript != null)
                 {
-                    // ADD THIS LINE
-                    playerScript.NotifyMushroomBounce();
-
                     rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
                     rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
 
                     playerScript.isGrounded = false;
                     playerScript.jumpsRemaining = 1;
+                    playerScript.DisableVariableJump();
 
                     if (mushroomAnim != null)
-                    {
                         mushroomAnim.SetTrigger(mushroomTrigger);
-                    }
                 }
             }
         }
