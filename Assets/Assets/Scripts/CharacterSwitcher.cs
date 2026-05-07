c1fd3214eec9d4b6c1bf971a6d769670e5b1e5c1
azizsarahmaria <azizsarahmaria@gmail.com>
2026-05-07 19:42:45 +0300

    uii

--- Assets/Assets/Scripts/CharacterSwitcher.cs
+++ Assets/Assets/Scripts/CharacterSwitcher.cs
@@ -1,19 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;  // ? Add this
-
 public class CharacterSwitcher : MonoBehaviour
 {
     public GameObject character1;
     public GameObject character2;
-
     private int activeCharacter = 1;
-
     void Start()
     {
         character1.SetActive(true);
         character2.SetActive(false);
     }
-
     void Update()
     {
         // ? Replace Input.GetKeyDown with Keyboard.current
@@ -23,22 +19,18 @@ public class CharacterSwitcher : MonoBehaviour
             SwitchCharacter();
         }
     }
-
     void SwitchCharacter()
     {
         GameObject current = activeCharacter == 1 ? character1 : character2;
         GameObject next = activeCharacter == 1 ? character2 : character1;
-
         // Sync transform
         next.transform.position = current.transform.position;
         next.transform.rotation = current.transform.rotation;
-
         // Sync Rigidbody2D velocity (optional but recommended)
         Rigidbody2D rbCurrent = current.GetComponent<Rigidbody2D>();
         Rigidbody2D rbNext = next.GetComponent<Rigidbody2D>();
         if (rbCurrent != null && rbNext != null)
             rbNext.linearVelocity = rbCurrent.linearVelocity;
-
         current.SetActive(false);
         next.SetActive(true);
         activeCharacter = activeCharacter == 1 ? 2 : 1;
